e26ef82b7a5fe21e0f4cf1c014d37d35a13116f1
JakovSudar <jakovusdar@gmail.com>
2020-02-27 22:20:27 +0100

    api integrated

--- application.py
+++ application.py
@@ -5,6 +5,7 @@ import json
 from flask_cors import CORS
 from flask import Flask
 import imutils
+import urllib
 
 application = Flask(__name__)
 CORS(application, support_credentials=True)
@@ -35,8 +36,37 @@ def getFeatures(url):
         "Correlation": result[5],
         "Contrast" : result[6],
         "Homogenity": result[7]
-    }   
-    x= json.dumps(obj)    
-    return x
+    }       
+    data =  {
+        "Inputs": {
+                "input1":
+                {
+                    "ColumnNames": ["Class", "R-avg", "G-avg", "B-avg", "Shape", "Energy", "Correlation", "Contrast", "Homogenity"],
+                    "Values":  [ "-", result[0], result[1], result[2], result[3],result[4], result[5], result[6], result[7]]
+                },        },
+            "GlobalParameters": {
+                    }
+    }
+    body = str.encode(json.dumps(data))
+    url = 'https://ussouthcentral.services.azureml.net/workspaces/3381f16d57184d1e9b30dea2f1e70257/services/84458a21acc641ab9a4c2c64bb732820/execute?api-version=2.0&details=true'
+    api_key = 'SR2Adf4SDvnfAOEh1vNvgYUIj+/ZKWZlju/mKscwFK5qpUZl4ItU89jWEIZnvkou4tHhTdWc2AOwsL6xcdO7wQ==' # Replace this with the API key for the web service
+    headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
+    req = urllib.Request(url, body, headers) 
+    try:
+        response = urllib.urlopen(req)
+        # If you are using Python 3+, replace urllib2 with urllib.request in the above code:
+        # req = urllib.request.Request(url, body, headers) 
+        # response = urllib.request.urlopen(req)
+        result = response.read()
+        print(result) 
+        return result
+    except urllib.HTTPError as error:
+        print("The request failed with status code: " + str(error.code))
+
+        # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
+        print(error.info())
+        print(json.loads(error.read()))    
+
+        return "error"
 if __name__ == "__main__":
     application.run()
